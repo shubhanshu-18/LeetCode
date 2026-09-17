84255df8068fb4981423adcf6b5c798caaf63a81
SHUBHANSHU TRIPATHI <shubhtripathi1438@gmail.com>
2026-09-18 00:26:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (59.66%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:
+    int countSubstrings(string s){
+        int n=s.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int l=i,r=i;
+            while(l>=0&&r<n&&s[l]==s[r]){
+                ans++;
+                l--;
+                r++;
+            }
+            l=i;
+            r=i+1;
+            while(l>=0&&r<n&&s[l]==s[r]){
+                ans++;
+                l--;
+                r++;
+            }
+        }
+        return ans;
+    }
+};

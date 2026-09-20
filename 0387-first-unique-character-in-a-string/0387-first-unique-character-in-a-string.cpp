4b4d8a98c843a47580daabce63c6821aadd00b29
SHUBHANSHU TRIPATHI <shubhtripathi1438@gmail.com>
2026-09-20 11:13:29 +0530

    Time: 0 ms (100%), Space: 14.6 MB (71.36%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int>hash(26,0);
+
+        for (int i=0;i<s.size();i++){
+            hash[s[i]-'a']++;
+
+        }
+        for (int i=0;i<s.size();i++){
+            if(hash[s[i]-'a']==1){
+                return i;
+            }
+
+        }
+        return -1;
+
+    }
+};
